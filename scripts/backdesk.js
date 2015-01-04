67c728b1122bf2ba7f4ce8a91746ce4d4b77c4fb
backdesk <crungmungus@gmail.com>
2015-01-04 11:59:10 +0000

    publish gh-pages (auto)

--- scripts/backdesk.js
+++ scripts/backdesk.js
@@ -4,4 +4,12 @@ $(function () {
 
 		items.height((items.height() === 0) ? 'auto' : 0);
 	});
+
+	$('a[href^="mailto:"]').on('click', function () {
+		var href = $(this).attr('href').substring(7);
+
+		if(ga) {
+			ga('send', 'event', { eventCategory: 'Contact', eventAction: 'Email', eventLabel: 'Contact Link', eventValue : href});
+		}
+	});
 });
